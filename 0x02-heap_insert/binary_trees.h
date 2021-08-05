e49458119137e10b5c0bad98bcacd0e23ddb456c
luismvargasg <1610@holbertonschool.com>
2021-08-05 15:27:24 -0500

    trying to guess what the checker wants

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *node_swap(heap_t *node);
 void binary_tree_print(const binary_tree_t *);
 
 
