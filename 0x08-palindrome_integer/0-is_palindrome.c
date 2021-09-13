998c2f7222e935ccc8a6f779f3a8846608cd08c7
luismvargasg <1610@holbertonschool.com>
2021-09-13 11:55:49 -0500

    adding 0x08 Palindrome project

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "palindrome.h"
+#include <stdio.h>
+
+
+/**
+ * is_palindrome - Function that checks whether or not
+ * a given unsigned integer is a palindrome.
+ * @n: is the number to be checked.
+ *
+ * Return: 1 if n is a palindrome, and 0 otherwise.
+ */
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long q = n, rem, result;
+
+	while (q != 0)
+	{
+		rem = q % 10;
+		result = result * 10 + rem;
+		q = q / 10;
+	}
+
+	if (result == n)
+		return (1);
+	return (0);
+}
