72b30db9be11550ce3209b0c4c4da2611ea6af15
luismvargasg <1610@holbertonschool.com>
2022-04-17 23:04:02 -0500

    adding rain project

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Given a list of non-negative integers representing the heights
+of walls with unit width 1, as if viewing the cross-section
+of a relief map, calculate how many square units of water
+will be retained after it rains.
+"""
+
+
+def rain(walls):
+    """ calculate how much water will be retained after it rains.
+    Arg:
+        - walls: is a list of non-negative integers.
+    Return: Integer indicating total amount of rainwater retained.
+    """
+    max_water = 0
+
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        max_water = max_water + (min(left, right) - walls[i])
+
+    return max_water
