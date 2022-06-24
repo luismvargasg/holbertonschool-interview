d97d9214664fd90a74abd61033c1dc0fbb3f914b
luismvargasg <1610@holbertonschool.com>
2022-06-23 22:05:29 -0500

    fixing efficiency

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -18,20 +18,12 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    newVal = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
         return -1
-
-    return store[total]
